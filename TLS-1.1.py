94a4c5bf841b04cf1afefab4c2ede6b5e0352433
Kerry <115020015+KerryNguyen@users.noreply.github.com>
2026-08-01 13:23:56 -0400

    Add files via upload

---
+++ TLS-1.1.py
@@ -0,0 +1,67 @@
+import os, ssl, pyfiglet, socket, warnings, time
+
+os.system('cls')
+
+print(pyfiglet.figlet_format("TLS-1.1", font="smblock"))
+
+# Used to suppress deprecation warnings, as this will fill up the screen.
+# DeprecationWarning: ssl.TLSVersion.TLSv1_1 is deprecated
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+server = "3.17.14.10"
+port = 6768
+key = os.path.join(os.getcwd(), "key.pem")
+cert = os.path.join(os.getcwd(), "cert.pem")
+
+while True:
+ user_input = input("How many trials? ")
+ if user_input.isdigit() and int(user_input) > 0:
+    req_trials = int(user_input)
+    break
+else:
+   print("Enter a number.")
+
+print()
+
+# Make sure you've opened an OpenSSL server, or else it'll actively refuse
+# Go into CMD or Powershell and enter:
+# openssl s_server -accept 6768 -cert cert.pem -key key.pem -min_protocol TLSv1.1 -cipher "ALL:@SECLEVEL=0
+# raw_sock = socket.create_connection(("localhost", port))
+
+# Setting up the settings for the client
+# We're setting it to ONLY use TLS 1.1
+ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ctx.minimum_version = ssl.TLSVersion.TLSv1_1
+ctx.maximum_version = ssl.TLSVersion.TLSv1_1
+
+# This should unblock the old TLS 1.1 ciphers and disable security checks
+# Disable Certificate Checking since our Certificate is self-signed and would FAIL
+ctx.set_ciphers("DEFAULT:@SECLEVEL=0")
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# A list to keep track of the measured time
+# variable to keep track of the handshake's number
+times = []
+num = 0
+timesheet = 'TLS-1.1.txt'
+
+# time.perf is used because it's precise.
+# We're wrapping it around the tls creation to measure the tls handshake. 
+with open(timesheet, 'w') as log:
+   
+    for trials in range(req_trials):
+        num += 1
+        raw_sock = socket.create_connection((server, port))
+        start = time.perf_counter()
+        tls_sock = ctx.wrap_socket(raw_sock, server_hostname=server)
+        end = time.perf_counter()
+        elapsed_ms = (end - start) * 1000
+        print(f"Handshake #{num}: {elapsed_ms:.3f}", file=log)
+        times.append(elapsed_ms)
+        tls_sock.close()
+
+    print(f"\nResults:", file=log)
+    print(f"Average: {sum(times) / len(times):.3f} ms", file=log)
+    print(f"Min: {min(times):.3f} ms", file=log)
+    print(f"Max: {max(times):.3f} ms", file=log)
